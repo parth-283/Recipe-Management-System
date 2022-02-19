fe3a4a511b1bda2f707231365e04d2f5780471b1
parth-283 <kp2832002@gmail.com>
2022-02-19 09:06:51 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import SignIn from './Component/SignUp&SignIn/SignIn';
 import SignUp from './Component/SignUp&SignIn/SignUp';
 import Recipe from './Recipes/Recipe.js';
 import PizzaMuffins from './Recipes/PizzaMuffins';
+import Breakfast from './Recipes/Category/Breakfast';
+import Lunch from './Recipes/Category/Lunch';
+import Dinner from './Recipes/Category/Dinner';
+import Dessert from './Recipes/Category/Dessert';
 
 function App() {
   return (
@@ -24,7 +28,10 @@ function App() {
           <Route path="/SignIn" element={<SignIn />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/recipe" element={<Recipe />} />
-          <Route path="/pizzaMuffins" element={<PizzaMuffins />} />
+          <Route path="/Breakfast" element={<Breakfast />} />
+          <Route path="/Lunch" element={<Lunch />} />
+          <Route path="/Dinner" element={<Dinner />} />
+          <Route path="/Dessert" element={<Dessert />} />
         </Route>
 
         <Route path="/admin" element={<Admin />}>
--- src/Component/Home/category.js
+++ src/Component/Home/category.js
@@ -1,143 +1,145 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import ButtonBase from '@mui/material/ButtonBase';
-import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import ButtonBase from "@mui/material/ButtonBase";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router";
 
 const images = [
   {
-    url: 'https://www.edamam.com/web-img/b24/b240799c96852042a317293299963392.jpg',
-    title: 'Breakfast',
-    width: '25%',
+    url: "https://www.edamam.com/web-img/b24/b240799c96852042a317293299963392.jpg",
+    title: "Breakfast",
+    width: "25%",
   },
   {
-    url: 'https://www.edamam.com/web-img/be9/be98aafdbc8aa7e3901405890eb46316',
-    title: 'Lunch',
-    width: '25%',
+    url: "https://www.edamam.com/web-img/be9/be98aafdbc8aa7e3901405890eb46316",
+    title: "Lunch",
+    width: "25%",
   },
   {
-    url: 'https://www.edamam.com/web-img/fb3/fb3cd151d676a71fa19d0e18a931bfc4',
-    title: 'Dinner',
-    width: '25%',
+    url: "https://www.edamam.com/web-img/fb3/fb3cd151d676a71fa19d0e18a931bfc4",
+    title: "Dinner",
+    width: "25%",
   },
   {
-    url: 'https://www.edamam.com/web-img/2de/2defa523fbf3b13a4598faf087191549.jpg',
-    title: 'dessert',
-    width: '25%',
+    url: "https://www.edamam.com/web-img/2de/2defa523fbf3b13a4598faf087191549.jpg",
+    title: "Dessert",
+    width: "25%",
   },
 ];
 
 const ImageButton = styled(ButtonBase)(({ theme }) => ({
-  position: 'relative',
+  position: "relative",
   height: 200,
-  [theme.breakpoints.down('sm')]: {
-    width: '100% !important', // Overrides inline-style
+  [theme.breakpoints.down("sm")]: {
+    width: "100% !important", // Overrides inline-style
     height: 100,
-
-
   },
-  '&:hover, &.Mui-focusVisible': {
+  "&:hover, &.Mui-focusVisible": {
     zIndex: 1,
-    '& .MuiImageBackdrop-root': {
+    "& .MuiImageBackdrop-root": {
       opacity: 0.15,
     },
-    '& .MuiImageMarked-root': {
+    "& .MuiImageMarked-root": {
       opacity: 0,
     },
-    '& .MuiTypography-root': {
-      border: '4px solid currentColor',
+    "& .MuiTypography-root": {
+      border: "4px solid currentColor",
     },
   },
 }));
 
-const ImageSrc = styled('span')({
-  position: 'absolute',
+const ImageSrc = styled("span")({
+  position: "absolute",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center 40%',
+  backgroundSize: "cover",
+  backgroundPosition: "center 40%",
 });
 
-const Image = styled('span')(({ theme }) => ({
-  position: 'absolute',
+const Image = styled("span")(({ theme }) => ({
+  position: "absolute",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
   color: theme.palette.common.white,
 }));
 
-const ImageBackdrop = styled('span')(({ theme }) => ({
-  position: 'absolute',
+const ImageBackdrop = styled("span")(({ theme }) => ({
+  position: "absolute",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
   backgroundColor: theme.palette.common.black,
   opacity: 0.4,
-  transition: theme.transitions.create('opacity'),
+  transition: theme.transitions.create("opacity"),
 }));
 
-const ImageMarked = styled('span')(({ theme }) => ({
+const ImageMarked = styled("span")(({ theme }) => ({
   height: 3,
   width: 18,
   backgroundColor: theme.palette.common.white,
-  position: 'absolute',
+  position: "absolute",
   bottom: -2,
-  left: 'calc(50% - 9px)',
-  transition: theme.transitions.create('opacity'),
+  left: "calc(50% - 9px)",
+  transition: theme.transitions.create("opacity"),
 }));
 
-export default function Category(props) {
+export default function Category({
+  title,
+  calories,
+  recipeimage,
+  ingredients,
+}) {
+  const navigate = useNavigate();
+  const recipedata = { title, calories, recipeimage, ingredients };
 
-    const navigate = useNavigate();
-    
+  console.log("recipedata", recipedata);
 
   return (
-      <div>  
-       
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
-        
-
-      {images.map((image) => (
-        <ImageButton
-          focusRipple
-          key={image.title}
-          style={{
-            width: image.width,
-          }}
-          
-        >
-           <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
-         
-          <ImageBackdrop className="MuiImageBackdrop-root" />
-          <Image>
-            <Typography
-              component="span"
-              variant="subtitle1"
-              color="inherit"
-              sx={{
-                position: 'relative',
-                p: 4,
-                pt: 2,
-                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-              }}
-              onClick={()=>navigate(`/${image.title}`)}
-            >
-              {image.title}
-              <ImageMarked className="MuiImageMarked-root" />
-            </Typography>
-          </Image>
-        </ImageButton>
-      ))}
-    </Box></div>
+    <div>
+      <Box
+        sx={{ display: "flex", flexWrap: "wrap", minWidth: 300, width: "100%" }}
+      >
+        {images.map((image) => (
+          <ImageButton
+            focusRipple
+            key={image.title}
+            style={{
+              width: image.width,
+            }}
+          >
+            <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+            <ImageBackdrop className="MuiImageBackdrop-root" />
+            <Image>
+              <Typography
+                component="span"
+                variant="subtitle1"
+                color="inherit"
+                sx={{
+                  position: "relative",
+                  p: 4,
+                  pt: 2,
+                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                }}
+                onClick={() =>
+                  navigate(`/${image.title}`, { state: { recipedata } })
+                }
+              >
+                {image.title}
+                <ImageMarked className="MuiImageMarked-root" />
+              </Typography>
+            </Image>
+          </ImageButton>
+        ))}
+      </Box>
+    </div>
   );
 }
-
-
