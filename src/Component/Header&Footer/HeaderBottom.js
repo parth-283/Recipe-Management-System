0938c0a2edd8e25b66f8aa5231a340eb081e0e29
parth-283 <kp2832002@gmail.com>
2022-03-15 10:25:43 +0530

    category issue

--- src/Component/Header&Footer/HeaderBottom.js
+++ src/Component/Header&Footer/HeaderBottom.js
@@ -7,7 +7,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
 import SearchIcon from "@material-ui/icons/Search";
 import Recipe from "./../../Recipes/Recipe";
-import Category from "../Home/Category";
+import Category from "../Home/category";
 
 const useStyles = makeStyles((theme) => ({
   root: {
